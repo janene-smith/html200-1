66cd49e3f7722dc6d1e8c71aa3ba78dd8dcebb3a
janene-smith <j.smith.peachy.bit@gmail.com>
2022-03-13 14:49:34 -0700

    exercise-three.js assignment results

---
+++ lesson03/exercise-three.js
@@ -0,0 +1,14 @@
+/* lesson3-challenge3 exercise-three.js */
+const zoo = [', giraffe', ', elephant', ', zebra', ', hippo', ', antelope'];
+
+zoo.forEach(function(animal) {
+
+    for (let i = 0; i< zoo.length; i++);
+      console.log(zoo.indexOf(animal), (animal));
+})
+
+// 0 ", giraffe"
+// 1 ", elephant"
+// 2 ", zebra"
+// 3 ", hippo"
+// 4 ", antelope"
