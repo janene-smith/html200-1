a830c38a473a989fb2ed48f86d8cbc794c39a423
janene-smith <j.smith.peachy.bit@gmail.com>
2022-03-20 23:14:25 -0700

    Initial JavaScript file

---
+++ lesson04/exercise-one.js
@@ -0,0 +1,9 @@
+function squareOfNum( ) {
+  let input = prompt('Enter a number');
+  let squareOfNum = Math.pow(num,2);
+  let result = 0;
+
+  result = squareOfNum * Number(input);
+
+  alert(result);
+}
