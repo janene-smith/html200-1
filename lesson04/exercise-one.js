00b7c124fe91ed8efe22a3afd7aa4c925073a860
janene-smith <j.smith.peachy.bit@gmail.com>
2022-03-21 23:00:20 -0700

    Added extra challenge function

--- lesson04/exercise-one.js
+++ lesson04/exercise-one.js
@@ -7,3 +7,12 @@ function squareNumber() {
   alert(squareOfNum);
 /*  console.log(squareOfNum); */
 }
+
+function twoNumbers() {
+  let num1 = prompt('Enter one number: ');
+  let num2 = prompt('Enter a second number:');
+  let multiplyNum = num1 * num2;
+
+  alert(multiplyNum);
+/*  console.log(multiplyNum); */
+}
