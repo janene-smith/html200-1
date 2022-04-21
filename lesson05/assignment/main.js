842a0022496c125c3f47bb67853628d8ed782b43
janene-smith <j.smith.peachy.bit@gmail.com>
2022-04-20 22:45:08 -0700

    Added while loop for input and system.exit(0) to main.js file

--- lesson05/assignment/main.js
+++ lesson05/assignment/main.js
@@ -9,88 +9,37 @@ The user will see their balance. Afterwards, they should be able to type another
 The balance should update after any Withdrawal or Deposit transactions.
 The program will loop asking for input until the user enters Q.
 */
+let balance = 0;
 
-//Using SWITCH
-function bankAppFunction() {
+function everything() {
+  while(true)
+  {
+     const myTrans = prompt('What type of transaction would you like to make?');
 
-let myTrans = document.getElementById("What type of transaction would you like to make?").value;
-myTrans = parseInt(myTrans);
-const balance = true;
-let deposit = 0;
-let withdraw = 0;
 
-switch (true) {
-
-  case userinput <= 10:
-    alert("Less than 10");
-    break;
-  case userinput > 10:
-}
-    switch (myTrans) {
-        case balance  {
-          if (myBalance) {
-            console.log('Your balance is: ${numb}');
-          } else {
-            console.log('Transaction not valid');
-          }
+      switch(myTrans) {
+         case 'B':
             alert(balance);
             break;
-        }
-        case 'D': {
-          function myDeposit() {
-            let myTrans = prompt('Enter amount to deposit');
-            let depAmt = 0;
-            if (depAmt >= 1 and < 50000) {
-              alert('Deposit amount of ${depAmt} has been received.');
-              console.log('Deposit amount of ${depAmt} has been received.');
-            } else {
-              alert(deposit)
-            }
-
-          }
-            return 'Enter an amount to deposit.';
-        }
-        case 'withdraw': {
-            return 'Enter an amount to withdraw.';
-        }
-        case 'quit': {
-          const Q = true;
-
-          if (Q) {
-            console.log('Program has been terminated.')
-          } else {
-            console.log('Transaction not valid.');
-          }
-            return 'Quit the program immediately.';
-        }
-        default: {
-            return 'transaction type not found';
-        }
+         case 'D':
+           let deposit = prompt("How much do you want to deposit?");
+           balance += parseInt(deposit);
+           if (deposit > 50000) {
+             alert('Depoist cannot exceed $50,000.')
+           } else if (deposit <= 50000) {
+             alert(balance);
+           }
+           break;
+         case 'W':
+           let withdraw = prompt("How much do you want to withdraw?");
+           balance -= parseInt(withdraw);
+           alert(balance);
+           break;
+         case 'Q':
+          system.exit(0);
+            //const quit = () => {
+              //return;
+            //}
+       }
     }
 }
-
-console.log(
-  transType(transaction)
-);
-*/
-
-// Alerts and Prompts - Lesson 5
-
-// Ask for input using a dialog box
-const myTrans = prompt('What type of transaction would you like to make?');
-
-// Display input in a dialog box
-alert(myTrans);
-
-// Ask for input with no default value
-const w = prompt('How much would you like to withdraw from your account today?');
-const d = prompt('How much would you like to deposit into your account today?');
-const b = ('Your balance is: ');
-
-// Display input in a dialog box
-alert (w);
-alert (d);
-alert (b);
-console.log(w);
-console.log(d);
-console.log(b);
